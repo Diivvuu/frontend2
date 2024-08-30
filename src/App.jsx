ff2bbde4ab3eba3b566a754adfff228232566961
Diivvuu <divyanshubawa9@gmail.com>
2024-08-30 22:18:58 +0530

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import SubscribeForm from './components/SubscribeForm';
 
 const App = () => {
   return (
-    <BrowserRouter basename="frontend2">
+      <
+          BrowserRouter basename
+    >
       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-        <Routes>
+       
+              <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/subscribe" element={<SubscribeForm />} />
         </Routes>
